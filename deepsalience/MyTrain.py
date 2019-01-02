1182166a6b38c162b36f4d56ab34c9801b8c251f
gargrohin <gargrohin0@gmail.com>
2019-01-03 03:08:21 +0530

    first model training fail

---
+++ deepsalience/MyTrain.py
@@ -0,0 +1,44 @@
+import keras
+from keras.models import Model
+from keras.layers import Dense, Input, Reshape, Lambda
+from keras.layers.convolutional import Conv2D
+from keras.layers.normalization import BatchNormalization
+from keras import backend as K
+import os
+import numpy as np
+
+#import experiment
+import core
+
+def model_def():
+    ### DEFINE MODEL ###
+    input_shape = (3633, 600, 6)
+    inputs = Input(shape=input_shape)
+
+    y1 = Conv2D(128, (5, 5), padding='same', activation='relu', name='bendy1')(inputs)
+    y1a = BatchNormalization()(y1)
+    y2 = Conv2D(64, (5, 5), padding='same', activation='relu', name='bendy2')(y1a)
+    y2a = BatchNormalization()(y2)
+    y3 = Conv2D(64, (3, 3), padding='same', activation='relu', name='smoothy1')(y2a)
+    y3a = BatchNormalization()(y3)
+    y4 = Conv2D(64, (3, 3), padding='same', activation='relu', name='smoothy2')(y3a)
+    y4a = BatchNormalization()(y4)
+    y5 = Conv2D(8, (70, 3), padding='same', activation='relu', name='distribute')(y4a)
+    y5a = BatchNormalization()(y5)
+    y6 = Conv2D(1, (1, 1), padding='same', activation='sigmoid', name='squishy')(y5a)
+    predictions = Lambda(lambda x: K.squeeze(x, axis=3))(y6)
+
+    model = Model(inputs=inputs, outputs=predictions)
+    return model
+
+
+#tracks = [daisy,pop,jazz,midi,opera]
+tracks = ["daisy"]
+data = np.load("../output2/training_data/daisy1.npz")
+xtrain = np.array([data['data_in'].T])
+#print(xtrain.shape)
+ytrain = np.array([data['data_out'].T])
+#print(ytrain.shape)
+model = model_def()
+model.compile( loss=core.bkld, metrics=['mse', core.soft_binary_accuracy], optimizer='adam' )
+model.fit(xtrain,ytrain)
